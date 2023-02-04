a4ee6e3822524ef66e01cfd7d75d52f8bd79ffe2
ShirshakShahi <royalshirshal000@gmail.com>
2023-02-04 23:31:18 +0545

    hi

--- basics of web development/js/clock.js
+++ basics of web development/js/clock.js
@@ -1,19 +1,17 @@
-let hour = document.getElementsByClassName("hr");
-let min = document.getElementsByClassName("min");
-let sec = document.getElementsByClassName("sec");
 
 
-let showTime = () => {
 
+function showTime() {
+    let hour = document.getElementsByClassName("hr");
+    let min = document.getElementsByClassName("min");
+    let sec = document.getElementsByClassName("sec");
+    let date = new Date();
     let h = date.getHours();
     let m = date.getMinutes();
     let s = date.getSeconds();
     hour[0].innerHTML = `${h} hours`;
     min[0].innerHTML = `${m} minutes`;
     sec[0].innerHTML = `${s} seconds`;
-
-
-
-
-    setTimeout
+    setTimeout(showTime, 1000)
 }
+showTime();
